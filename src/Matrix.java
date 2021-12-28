fdca16bfca047c237b8b702086b4b3dbd3771312
Ming DeMers (Linux) <mtd64@cornell.edu>
2021-12-28 16:18:26 -0500

    Added classes, some work on Matrix class

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class Matrix {
+import java.util.ArrayList;
+
+/** Class containing all methods to create and manipulate matrices. *
+ */
+public class Matrix {
+
+    int xSize, ySize;
+
+    /** Constructor to create a new matrix, of x and y size
+     *
+     * @param x
+     * @param y
+     */
+    public Matrix (int x, int y) {
+        xSize= x;
+        ySize= y;
+
+    }
+
+    /** Returns the sum of two given matrices, a and b.
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public ArrayList add(ArrayList a, ArrayList b) {
+        ArrayList sum= new ArrayList();
+
+        return sum;
+    }
+
+    /** Returns the product of two matrices, a and b.
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public ArrayList multiply(ArrayList a, ArrayList b) {
+        ArrayList product= new ArrayList();
+        return product;
+    }
 }
